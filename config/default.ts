9197f0752fd11a423e91eab504245c02acc552a5
Natasha Limeres <natasha.limeres@mktsystems.com>
2023-12-06 10:32:06 -0300

    Connecting to the database and configuring environment variables

--- config/default.ts
+++ config/default.ts
@@ -1,3 +1,6 @@
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASS;
 export default {
   port: 3000,
+  dbUrl: `mongodb+srv://${dbUser}:${dbPassword}@cluster0.no36x54.mongodb.net/`,
 };
